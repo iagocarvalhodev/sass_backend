738d87e371570f4842e52880a28ed5d395c3f975
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-09-10 16:39:44 -0300

    session controller e models e seeder team

--- app/Models/Team.js
+++ app/Models/Team.js
@@ -1,14 +1,26 @@
-"use strict";
+'use strict'
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use("Model");
+const Model = use('Model')
 
 class Team extends Model {
-  users() {
-    return this.belongsToMany("App/Models/User").pivotModel(
-      "App/Models/UserTeam"
-    );
+  static boot () {
+    super.boot()
+
+    this.addTrait('@provider:Lucid/Slugify', {
+      fields: {
+        slug: 'name'
+      },
+      strategy: 'dbIncrement',
+      disableUpdates: false
+    })
+  }
+
+  users () {
+    return this.belongsToMany('App/Models/User').pivotModel(
+      'App/Models/UserTeam'
+    )
   }
 }
 
-module.exports = Team;
+module.exports = Team
